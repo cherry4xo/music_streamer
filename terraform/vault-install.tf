6bfaeb7a8db4b17142eb932330f36aa2b74eaa8c
cherry4xo <1jlaketuk1@gmail.com>
2025-07-01 14:29:44 +0300

    fix vault deploying

--- terraform/vault-install.tf
+++ terraform/vault-install.tf
@@ -1,7 +1,6 @@
 resource "helm_release" "vault" {
   name = "vault"
-  repository = "https://helm.releases.hashicorp.com"
-  chart = "vault"
+  chart = "./vault-helm"
   namespace = "default"
 
   values = [
