de469967e4c682793ef3512133e96ceb69ca186a
cherry4xo <1jlaketuk1@gmail.com>
2025-07-08 15:17:06 +0300

    fix set arguments

--- terraform/platform.tf
+++ terraform/platform.tf
@@ -12,34 +12,38 @@ resource "helm_release" "postgres" {
   chart = "postgresql"
   version = "13.2.27"
 
-  set {
-    name  = "auth.postgresPassword"
-    value = data.vault_generic_secret.postgres_credentials.data["admin_password"]
-  }
-  set {
-    name  = "auth.databases[0]"
-    value = data.vault_generic_secret.postgres_credentials.data["auth_db_name"]
-  }
-  set {
-    name  = "auth.usernames[0]"
-    value = data.vault_generic_secret.postgres_credentials.data["auth_db_user"]
-  }
-  set {
-    name  = "auth.passwords[0]"
-    value = data.vault_generic_secret.postgres_credentials.data["auth_db_pass"]
-  }
-  set {
-    name  = "auth.databases[1]"
-    value = data.vault_generic_secret.postgres_credentials.data["account_db_name"]
-  }
-  set {
-    name  = "auth.usernames[1]"
-    value = data.vault_generic_secret.postgres_credentials.data["account_db_user"]
-  }
-  set {
-    name  = "auth.passwords[1]"
-    value = data.vault_generic_secret.postgres_credentials.data["account_db_pass"]
-  }
+  set = [
+    {
+      name  = "auth.postgresPassword"
+      value = data.vault_generic_secret.postgres_credentials.data["admin_password"]
+    },
+    {
+      name  = "auth.databases[0]"
+      value = data.vault_generic_secret.postgres_credentials.data["auth_db_name"]
+    },
+    {
+      name  = "auth.usernames[0]"
+      value = data.vault_generic_secret.postgres_credentials.data["auth_db_user"]
+    },
+    {
+      name  = "auth.passwords[0]"
+      value = data.vault_generic_secret.postgres_credentials.data["auth_db_pass"]
+    },
+    {
+      name  = "auth.databases[1]"
+      value = data.vault_generic_secret.postgres_credentials.data["account_db_name"]
+    },
+    {
+      name  = "auth.usernames[1]"
+      value = data.vault_generic_secret.postgres_credentials.data["account_db_user"]
+    },
+    {
+      name  = "auth.passwords[1]"
+      value = data.vault_generic_secret.postgres_credentials.data["account_db_pass"]
+    }
+  ]
+
+  
 }
 
 resource "helm_release" "redis" {
@@ -48,10 +52,12 @@ resource "helm_release" "redis" {
   chart      = "redis"
   version    = "18.6.0"
 
-  set {
-    name  = "auth.password"
-    value = tostring(data.vault_generic_secret.redis_credentials.data["password"])
-  }
+  set = [
+    {
+      name  = "auth.password"
+      value = tostring(data.vault_generic_secret.redis_credentials.data["password"])
+    }
+  ]
 }
 
 resource "helm_release" "kafka" {
