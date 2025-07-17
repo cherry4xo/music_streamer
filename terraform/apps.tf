f51d71db883777d79a8849582b35eb2a8a5bb723
cherry4xo <1jlaketuk1@gmail.com>
2025-07-17 13:26:07 +0300

    hotfix

--- terraform/apps.tf
+++ terraform/apps.tf
@@ -34,7 +34,7 @@ resource "kubernetes_deployment" "app_deployments" {
           name = each.key
           image = "${var.ci_registry}/${var.ci_project_path}/${each.key}:${var.image_tag}"
 
-          image_pull_policy = "Always"
+          image_pull_policy = "Never"
 
           port {
             container_port = each.value.container_port
